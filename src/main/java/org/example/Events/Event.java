e503aa2d9e0fc9a0928fb8500308e8bff4887dba
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2025-05-08 18:17:11 +0200

    event first implementation

--- src/main/java/org/example/Events/Event.java
+++ src/main/java/org/example/Events/Event.java
@@ -1,4 +1,23 @@
 package org.example.Events;
 
 public abstract class Event {
+
+    private Match match;
+
+    private Matchstate backup;
+
+    Event(Match match) {
+        this.match = match;
+    }
+
+    void backup() {
+        backup = match.getState();
+    }
+
+    public void undo() {
+        match.setState(backup);
+    }
+
+    public abstract boolean execute();
+
 }
