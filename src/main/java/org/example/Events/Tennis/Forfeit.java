f90854ce376b57fcb91570992f5b44a511c3fabf
celiajosso <celia.josso@gmail.com>
2025-05-21 13:55:47 +0200

    start event

---
+++ src/main/java/org/example/Events/Tennis/Forfeit.java
@@ -0,0 +1,7 @@
+package org.example.Events.Tennis;
+
+import org.example.Events.Event;
+
+public class Forfeit extends Event {
+
+}
---
+++ src/main/java/org/example/Events/Tennis/GameCancel.java
@@ -0,0 +1,7 @@
+package org.example.Events.Tennis;
+
+import org.example.Events.Event;
+
+public class GameCancel extends Event {
+    
+}
